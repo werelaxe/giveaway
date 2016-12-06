de9b33cde84453ee2820938d1b4017511d780f31
Werelaxe <integrex@yandex.ru>
2016-12-07 01:09:43 +0500

    logic fixes

--- game.py
+++ game.py
@@ -8,50 +8,45 @@ class Game:
         self.current_player = BOTTOM_PLAYER
         self.selected_cells = []
         self.active_cell = None
-        self.cutting_cell = None
 
     def change_cell(self, cell):
         self.field[cell] += 1
         if self.field[cell] == 5:
             self.field[cell] = -4
 
+    def select_cell(self, step_cell):
+        selected_cells = self.field.get_selected_cells(self.field[step_cell], step_cell)
+        cut_cells = self.field.get_cut_cells(self.field[step_cell], step_cell)
+        if cut_cells:
+            self.selected_cells = cut_cells
+        else:
+            self.selected_cells = selected_cells
+        if selected_cells or cut_cells:
+            self.active_cell = step_cell
+        else:
+            self.active_cell = None
+
+    def do_cut(self, step_cell):
+        if step_cell in self.selected_cells:
+            self.field[step_cell] = self.field[self.active_cell]
+            self.field[self.active_cell] = EMPTY
+            if type(self.selected_cells) == dict:
+                self.field[self.selected_cells[step_cell]] = EMPTY
+            self.selected_cells = []
+            self.active_cell = None
+            if not self.field.get_cut_cells(self.field[step_cell], step_cell):
+                self.current_player += 1
+                if self.current_player == 5:
+                    self.current_player = 1
+
     def click(self, step_cell):
         if abs(self.field[step_cell]) == abs(self.current_player):
-            if self.cutting_cell is not None:
-                if self.cutting_cell != step_cell:
-                    return
-            selected_cells = self.field.get_selected_cells(self.field[step_cell], step_cell)
-            cut_cells = self.field.get_cut_cells(self.field[step_cell], step_cell)
-            if cut_cells:
-                self.selected_cells = cut_cells
-            else:
-                self.selected_cells = selected_cells
-            if selected_cells or cut_cells:
-                self.active_cell = step_cell
-            else:
-                self.active_cell = None
+            self.select_cell(step_cell)
         elif not self.field[step_cell]:
-            if self.cutting_cell is not None:
-                if self.active_cell != self.cutting_cell:
-                    return
-            if step_cell in self.selected_cells:
-                self.field[step_cell] = self.field[self.active_cell]
-                self.field[self.active_cell] = EMPTY
-                if type(self.selected_cells) == dict:
-                    self.field[self.selected_cells[step_cell]] = EMPTY
-                self.selected_cells = []
-                self.active_cell = None
-                if not self.field.get_cut_cells(self.current_player, step_cell):
-                    self.cutting_cell = None
-                    self.current_player += 1
-                    if self.current_player == 5:
-                        self.current_player = 1
-                else:
-                    self.cutting_cell = step_cell
-                    print(step_cell)
+            self.do_cut(step_cell)
 
     def update(self):
-        print('updating field: {}'.format(self))
+        print('Updating field: {}'.format(self))
 
 if __name__ == '__main__':
     game = Game(14)
